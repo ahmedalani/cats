dd61f9e68d3dff3fdf90677223c847083b23de3a
Ahmed Alani <s3leha@gmail.com>
2020-06-19 10:30:43 -0700

    initiated NPM, installed mocha + chai, all tests passing

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -1,20 +1,24 @@
 /* eslint-disable func-names, no-console */
 // asyncBreeds.js
 const fs = require('fs');
+
 const breedDetailsFromFile = function (breed, cbRunsAfterDataAcquired) {
   console.log('breedDetailsFromFile: Calling readFile...');
   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
     console.log("In readFile's Callback: it has the data.");
     // ISSUE: Returning from *inner* callback function, not breedDetailsFromFile.
-    if (!error) cbRunsAfterDataAcquired(data);
+    if (error) {
+      cbRunsAfterDataAcquired(undefined);
+    } else cbRunsAfterDataAcquired(data);
   });
   // ISSUE: Attempting to return data out here will also not work.
   //        Currently not returning anything from here, so breedDetailsFromFile function returns undefined.
 };
-const printOutBreed = (bread) => {
-  console.log('Return Value: ', bread); // => will NOT print out details, instead we will see undefined!
-  // console.log('yoo!!!, ', bread)
-}
+// const printOutBreed = (bread) => {
+//   console.log('Return Value: ', bread); // => will NOT print out details, instead we will see undefined!
+//   // console.log('yoo!!!, ', bread)
+// }
 
-// we try to get the return value
-breedDetailsFromFile('Bombay', printOutBreed);
+// // we try to get the return value
+// breedDetailsFromFile('Bombay', printOutBreed);
+module.exports = breedDetailsFromFile;
