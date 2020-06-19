08f087189665a33dfa656aa520089a04494372d5
Ahmed Alani <s3leha@gmail.com>
2020-06-19 09:20:41 -0700

    asyncBreeds.js works, new cb added to print out data

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -1,18 +1,20 @@
 /* eslint-disable func-names, no-console */
 // asyncBreeds.js
 const fs = require('fs');
-
-const breedDetailsFromFile = function (breed) {
+const breedDetailsFromFile = function (breed, cbRunsAfterDataAcquired) {
   console.log('breedDetailsFromFile: Calling readFile...');
   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
     console.log("In readFile's Callback: it has the data.");
     // ISSUE: Returning from *inner* callback function, not breedDetailsFromFile.
-    if (!error) return data;
+    if (!error) cbRunsAfterDataAcquired(data);
   });
   // ISSUE: Attempting to return data out here will also not work.
   //        Currently not returning anything from here, so breedDetailsFromFile function returns undefined.
 };
+const printOutBreed = (bread) => {
+  console.log('Return Value: ', bread); // => will NOT print out details, instead we will see undefined!
+  // console.log('yoo!!!, ', bread)
+}
 
 // we try to get the return value
-const bombay = breedDetailsFromFile('Bombay');
-console.log('Return Value: ', bombay); // => will NOT print out details, instead we will see undefined!
+breedDetailsFromFile('Bombay', printOutBreed);
